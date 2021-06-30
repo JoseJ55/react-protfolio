2f43162ace52f0b89d48e35b9f60fe3133400f1c
Jose Jimenez <josejimenez826@gmail.com>
2021-06-30 00:02:40 -0700

    Worked on about section

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,10 +4,10 @@ import "./style.css";
 function Navbar() {
     return (
         <div id="nav">
-            <button id="about" className="btn"><a href="/">About</a></button>
-            <button id="project" className="btn"><a href="/">Projects</a></button>
-            <button id="contact" className="btn"><a href="/">Contact</a></button>
-            <button id="resume" className="btn"><a href="/">Resume</a></button>
+            <button className="btn"><a href="/">About</a></button>
+            <button className="btn"><a href="/">Projects</a></button>
+            <button className="btn"><a href="/">Contact</a></button>
+            <button className="btn"><a href="/">Resume</a></button>
         </div>
     )
 }
